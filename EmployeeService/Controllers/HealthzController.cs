7d87b912e2abea8e84f0907bd6225f6307220c8b
huynhducthanhtuan <tuan40655@gmail.com>
2025-08-27 19:33:49 +0700

    Add healthz check APIs

---
+++ EmployeeService/Controllers/HealthzController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    [ApiController]
+    [Route("Healthz")]
+    public class HealthzController : ControllerBase
+    {
+        private readonly ILogger<HealthzController> _logger;
+
+        public HealthzController(ILogger<HealthzController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> HealthCheck()
+        {
+            return Ok("Service is alive!");
+        }
+    }
+}

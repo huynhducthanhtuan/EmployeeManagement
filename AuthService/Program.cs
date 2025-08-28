b16cf2ec35307d70ec57a3c0f821d65c540c5b35
huynhducthanhtuan <tuan40655@gmail.com>
2025-08-28 10:38:16 +0700

    Testing: Comment AWS Secrets Manager config

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -11,20 +11,20 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // AWS Secrets Manager config for production
-if (!builder.Environment.IsDevelopment())
-{
-    var secretJson = await AwsSecretHelper.GetSecretAsync("EmployeeManagementSecrets", "ap-southeast-1");
+//if (!builder.Environment.IsDevelopment())
+//{
+//    var secretJson = await AwsSecretHelper.GetSecretAsync("EmployeeManagementSecrets", "ap-southeast-1");
 
-    if (!string.IsNullOrEmpty(secretJson))
-    {
-        // Parse JSON secret to IConfiguration
-        var secretConfig = new ConfigurationBuilder()
-            .AddJsonStream(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(secretJson)))
-            .Build();
+//    if (!string.IsNullOrEmpty(secretJson))
+//    {
+//        // Parse JSON secret to IConfiguration
+//        var secretConfig = new ConfigurationBuilder()
+//            .AddJsonStream(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(secretJson)))
+//            .Build();
 
-        builder.Configuration.AddConfiguration(secretConfig);
-    }
-}
+//        builder.Configuration.AddConfiguration(secretConfig);
+//    }
+//}
 
 // AWS Cognito config
 builder.Services.Configure<CognitoOptions>(builder.Configuration.GetSection("Cognito"));

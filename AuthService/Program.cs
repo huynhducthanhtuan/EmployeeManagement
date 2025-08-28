36183eb550ec24c882cb2d86f7aa743ee165e252
huynhducthanhtuan <tuan40655@gmail.com>
2025-08-28 10:19:17 +0700

    Testing: Config read appsettings variables from AWS Secrets Manager for production

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -2,12 +2,29 @@ using System.Reflection;
 using Amazon;
 using Amazon.CognitoIdentityProvider;
 using AuthService.DTO;
+using AuthService.Helpers;
 using AuthService.Interfaces;
 using AuthService.Services;
 using InfraCore.Commons.Extensions;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// AWS Secrets Manager config for production
+if (!builder.Environment.IsDevelopment())
+{
+    var secretJson = await AwsSecretHelper.GetSecretAsync("EmployeeManagementSecrets", "ap-southeast-1");
+
+    if (!string.IsNullOrEmpty(secretJson))
+    {
+        // Parse JSON secret to IConfiguration
+        var secretConfig = new ConfigurationBuilder()
+            .AddJsonStream(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(secretJson)))
+            .Build();
+
+        builder.Configuration.AddConfiguration(secretConfig);
+    }
+}
 
 // AWS Cognito config
 builder.Services.Configure<CognitoOptions>(builder.Configuration.GetSection("Cognito"));

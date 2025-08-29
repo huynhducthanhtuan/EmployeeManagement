09a3083fd9e21a27c597d32389029ca322108c9c
huynhducthanhtuan <tuan40655@gmail.com>
2025-08-29 17:50:19 +0700

    InfraCore: Add RoleConstants class

---
+++ InfraCore/Commons/Constants/RoleConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace InfraCore.Commons.Constants
+{
+    public static class RoleConstants
+    {
+        public static string Employee = "Employee";
+        public static string Manager = "Manager";
+        public static string Admin = "Admin";
+    }
+}

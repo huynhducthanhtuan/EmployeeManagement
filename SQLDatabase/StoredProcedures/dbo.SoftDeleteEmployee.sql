d4260317ea4c4eada2f8e7274b63e93dc2f225a9
huynhducthanhtuan <tuan40655@gmail.com>
2025-09-02 12:48:46 +0700

    SQLDatabase: Update SP SoftDeleteEmployee logic

--- SQLDatabase/StoredProcedures/dbo.SoftDeleteEmployee.sql
+++ SQLDatabase/StoredProcedures/dbo.SoftDeleteEmployee.sql
@@ -1,6 +1,6 @@
 USE [EmployeeDB]
 GO
-/****** Object:  StoredProcedure [dbo].[SoftDeleteEmployee]    Script Date: 01-Sep-25 22:16:25 PM ******/
+/****** Object:  StoredProcedure [dbo].[SoftDeleteEmployee]    Script Date: 02-Sep-25 12:47:10 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -9,7 +9,6 @@ ALTER PROCEDURE [dbo].[SoftDeleteEmployee]
 	@EmployeeId NVARCHAR(450)
 AS
 BEGIN
-	UPDATE Employees 
-	SET IsDeleted = 1, UpdatedDate = GETDATE()
-	WHERE EmployeeId = @EmployeeId
+	UPDATE EmployeeProjects SET IsDeleted = 1, UpdatedDate = GETDATE() WHERE EmployeeId = @EmployeeId
+	UPDATE Employees SET IsDeleted = 1, UpdatedDate = GETDATE() WHERE EmployeeId = @EmployeeId
 END

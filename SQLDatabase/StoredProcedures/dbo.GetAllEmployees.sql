9daa87ee6c3e5f349558f56a1a8778096792b36b
huynhducthanhtuan <tuan40655@gmail.com>
2025-09-02 11:09:52 +0700

    SQLDatabase: Optimize SP GetAllEmployees

--- SQLDatabase/StoredProcedures/dbo.GetAllEmployees.sql
+++ SQLDatabase/StoredProcedures/dbo.GetAllEmployees.sql
@@ -1,6 +1,6 @@
 USE [EmployeeDB]
 GO
-/****** Object:  StoredProcedure [dbo].[GetAllEmployees]    Script Date: 01-Sep-25 22:15:38 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetAllEmployees]    Script Date: 02-Sep-25 11:02:48 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -9,17 +9,10 @@ ALTER PROCEDURE [dbo].[GetAllEmployees]
 	@IncludeDeleted BIT = 0
 AS
 BEGIN
-	IF @IncludeDeleted = 1
-		BEGIN
-			SELECT E.FullName, E.Gender, E.DateOfBirth, E.Hometown, E.AvatarImage, P.PositionName, D.DepartmentName FROM Employees E
-			JOIN Positions P ON P.PositionId = E.PositionId
-			JOIN Departments D ON D.DepartmentId = E.DepartmentId
-		END
-	ELSE
-		BEGIN
-			SELECT E.FullName, E.Gender, E.DateOfBirth, E.Hometown, E.AvatarImage, P.PositionName, D.DepartmentName FROM Employees E
-			JOIN Positions P ON P.PositionId = E.PositionId
-			JOIN Departments D ON D.DepartmentId = E.DepartmentId
-			WHERE E.IsDeleted = 0
-		END
+	BEGIN
+		SELECT E.FullName, E.Gender, E.DateOfBirth, E.Hometown, E.AvatarImage, P.PositionName, D.DepartmentName FROM Employees E
+		JOIN Positions P ON P.PositionId = E.PositionId
+		JOIN Departments D ON D.DepartmentId = E.DepartmentId
+		WHERE @IncludeDeleted = 1 OR E.IsDeleted = 0
+	END
 END

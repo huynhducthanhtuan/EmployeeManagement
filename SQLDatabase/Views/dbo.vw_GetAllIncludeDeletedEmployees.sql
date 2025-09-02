483063f4f34cbc27305c50bfe99a81e3b9dffca2
huynhducthanhtuan <tuan40655@gmail.com>
2025-09-02 13:09:38 +0700

    SQLDatabase: Add view vw_GetAllEmployees and vw_GetAllIncludeDeletedEmployees

---
+++ SQLDatabase/Views/dbo.vw_GetAllIncludeDeletedEmployees.sql
@@ -0,0 +1,7 @@
+ALTER VIEW vw_GetAllIncludeDeletedEmployees
+AS
+SELECT E.FullName, E.Gender, E.DateOfBirth, E.Hometown, E.AvatarImage, P.PositionName, D.DepartmentName 
+FROM Employees E
+JOIN Positions P ON P.PositionId = E.PositionId
+JOIN Departments D ON D.DepartmentId = E.DepartmentId
+WHERE E.IsDeleted = 0

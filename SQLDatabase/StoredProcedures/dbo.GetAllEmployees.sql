24974cf2a8cea9afbf092163d2696086f22c981d
huynhducthanhtuan <tuan40655@gmail.com>
2025-09-02 13:01:29 +0700

    SQLDatabase: Update SP GetAllEmployees

--- SQLDatabase/StoredProcedures/dbo.GetAllEmployees.sql
+++ SQLDatabase/StoredProcedures/dbo.GetAllEmployees.sql
@@ -1,6 +1,6 @@
 USE [EmployeeDB]
 GO
-/****** Object:  StoredProcedure [dbo].[GetAllEmployees]    Script Date: 02-Sep-25 11:02:48 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetAllEmployees]    Script Date: 02-Sep-25 12:59:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -9,10 +9,8 @@ ALTER PROCEDURE [dbo].[GetAllEmployees]
 	@IncludeDeleted BIT = 0
 AS
 BEGIN
-	BEGIN
-		SELECT E.FullName, E.Gender, E.DateOfBirth, E.Hometown, E.AvatarImage, P.PositionName, D.DepartmentName FROM Employees E
-		JOIN Positions P ON P.PositionId = E.PositionId
-		JOIN Departments D ON D.DepartmentId = E.DepartmentId
-		WHERE @IncludeDeleted = 1 OR E.IsDeleted = 0
-	END
+	SELECT E.FullName, E.Gender, E.DateOfBirth, E.Hometown, E.AvatarImage, P.PositionName, D.DepartmentName FROM Employees E
+	JOIN Positions P ON P.PositionId = E.PositionId
+	JOIN Departments D ON D.DepartmentId = E.DepartmentId
+	WHERE @IncludeDeleted = 1 OR E.IsDeleted = 0
 END
